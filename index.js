a2bed78f9a42911b027350a95d06d714d3e5cea0
Ghozy Nurcahyadi <M.Ghozy@ag-it.com>
2021-11-08 12:20:41 +0700

    Edit data console

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-const path = require('path')
-const async = require('async')
-const newman = require('newman')
+const path = require("path");
+const async = require("async");
+const newman = require("newman");
 
-const PARALLEL_RUN_COUNT = 1
+const PARALLEL_RUN_COUNT = 25;
 
 const parametersForTestRun = {
-    collection: path.join(__dirname, 'postman/postman_collection.json'), // your collection
-    // environment: path.join(__dirname, 'postman/localhost.postman_environment.json'), //your env
-    reporters: 'cli'
+  collection: path.join(__dirname, "postman/postman_collection.json"), // your collection
+  // environment: path.join(__dirname, 'postman/localhost.postman_environment.json'), //your env
+  reporters: "cli",
 };
 
 parallelCollectionRun = function (done) {
-    newman.run(parametersForTestRun, done);
+  newman.run(parametersForTestRun, done);
 };
 
-let commands = []
+let commands = [];
 for (let index = 0; index < PARALLEL_RUN_COUNT; index++) {
-    commands.push(parallelCollectionRun);
+  commands.push(parallelCollectionRun);
 }
 
-// Runs the Postman sample collection thrice, in parallel.
-async.parallel(
-    commands,
-    (err, results) => {
-        err && console.error(err);
+let failed = 0;
+let failedMessage = [];
+let success = 0;
 
-        results.forEach(function (result) {
-            var failures = result.run.failures;
-            console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
-                `${result.collection.name} ran successfully.`);
-        });
-    });
+// Runs the Postman sample collection thrice, in parallel.
+async.parallel(commands, (err, results) => {
+  err && console.error(err);
+
+  results.forEach(function (result, index) {
+    var failures = result.run.failures;
+    if (failures.length) {
+      failed = failed + 1;
+      failedMessage.push(
+        `Iteration ${index + 1} - ${failures[0].error.message}`
+      );
+    } else {
+      success = success + 1;
+    }
+  });
+  console.info("");
+  console.info("Collection Run Completed");
+  console.info("=========================");
+  console.info("SUMMARY");
+  console.info("");
+  console.info("Failed Scenario :", failed);
+  console.info("Success Scenario :", success);
+  console.info("=========================");
+  console.info("DETAIL INFORMATION");
+  console.info("");
+  console.info("Failed Reason :", JSON.stringify(failedMessage, null, 2));
+  console.info("=========================");
+});
