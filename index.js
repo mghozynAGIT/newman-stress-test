7211be865efc04307467f06c31d44e7c19c65b0e
Ghozy Nurcahyadi <M.Ghozy@ag-it.com>
2021-11-10 21:51:23 +0700

    set paralel run count default to 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const async = require("async");
 const newman = require("newman");
 
-const PARALLEL_RUN_COUNT = 25;
+const PARALLEL_RUN_COUNT = 1;
 
 const parametersForTestRun = {
   collection: path.join(__dirname, "postman/postman_collection.json"), // your collection
